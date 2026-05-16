e9e794a04225a52aa67755e00d37a4d23e94458f
tolqinovabdullox000-cell <tolqinovabdullox000@gmail.com>
2026-05-16 08:55:30 +0500

    xaxaxa

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const container = document.getElementById("todoContainer");
 
 let editTarget = null;
 
-// Todo yaratish
 function createTodoItem(text) {
   const newItem = document.createElement("div");
   newItem.className = "todo-item";
@@ -20,15 +19,17 @@ function createTodoItem(text) {
 
   const span = newItem.querySelector(".todo-text");
 
-  // Edit
+  span.onclick = () => {
+    span.classList.toggle("completed");
+  };
+
   newItem.querySelector(".edit-btn").onclick = () => {
     input.value = span.textContent;
-    btn.textContent = "Qo'shish";
+    btn.textContent = "O'zgartirish";
     editTarget = span;
     input.focus();
   };
 
-  // Delete
   newItem.querySelector(".delete-btn").onclick = () => {
     if (editTarget === span) {
       resetForm();
@@ -40,14 +41,12 @@ function createTodoItem(text) {
   container.appendChild(newItem);
 }
 
-// Formani tozalash
 function resetForm() {
   input.value = "";
   btn.textContent = "Qo'shish";
   editTarget = null;
 }
 
-// Qo‘shish yoki o‘zgartirish
 function handleAdd() {
   const text = input.value.trim();
 
@@ -62,10 +61,8 @@ function handleAdd() {
   resetForm();
 }
 
-// Button bosilganda
 btn.onclick = handleAdd;
 
-// Enter bosilganda
 input.addEventListener("keypress", (e) => {
   if (e.key === "Enter") {
     handleAdd();
